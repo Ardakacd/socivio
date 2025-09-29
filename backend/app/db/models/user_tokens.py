d865c7f7c3ecbde7a9417f422468626518664923
ardakabadayi <kabadayi_arda@hotmail.com>
2025-09-28 23:05:54 -0400

    make refresh token optional

--- backend/app/db/models/user_tokens.py
+++ backend/app/db/models/user_tokens.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
 from .base import Base
 import enum
+from typing import Optional
 
 class PlatformType(enum.Enum):
     youtube = "youtube"
     facebook = "facebook"
-    instagram = "instagram"
     tiktok = "tiktok"
 
 class UserTokenModel(Base):
@@ -19,7 +19,7 @@ class UserTokenModel(Base):
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     
     access_token: Mapped[str] = mapped_column(Text, nullable=False)
-    refresh_token: Mapped[str] = mapped_column(Text, nullable=False)
+    refresh_token: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
     expires_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), nullable=False)
     
     platform: Mapped[PlatformType] = mapped_column(Enum(PlatformType), nullable=False)
