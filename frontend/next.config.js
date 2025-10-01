27cc62a57cc18330ff82aeaf463b60bfa58304c1
ardakabadayi <kabadayi_arda@hotmail.com>
2025-10-01 13:27:29 -0400

    set react strict mode to false

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  reactStrictMode: false,
   images: {
     domains: ['scontent.cdninstagram.com', 'instagram.com', 'cdn.instagram.com'],
   },
